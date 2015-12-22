14576bfaa2d0f01ba7775925ed31a718f2e6d760
bbiggu68 <bbiggu68@naver.com>
2015-12-22 17:25:33 +0900

    멀티터치 스케일 동작 처리 기능 뼈대 추가.

--- app/src/main/java/org/homenet/woscilloscope/ScopePanel.java
+++ app/src/main/java/org/homenet/woscilloscope/ScopePanel.java
@@ -11,6 +11,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -22,14 +23,17 @@ public class ScopePanel extends SurfaceView implements SurfaceHolder.Callback {
     private final static String TAG = ScopePanel.class.getSimpleName();
     private static final boolean D = true;
     //
+    private MainActivity mCallerClone = null;
     Bitmap mBack;
     Pulse mPulse;
     SurfaceHolder mHolder;
     DrawThread mThread;
     Rect dstRect;
+    private ScaleGestureDetector mScaleGestureDetector;
 
     public ScopePanel(Context context) {
         super(context);
+        mCallerClone = (MainActivity)context;
         //mBack = BitmapFactory.decodeResource(context.getResources(), R.drawable.background);
         prepareBackGroundGrid();
         dstRect = new Rect(0, 0, 1919, 1053); // 가로 모드일 때, 1920 * 1054 픽셀이 가용한 그리기 영역임.
@@ -39,6 +43,8 @@ public class ScopePanel extends SurfaceView implements SurfaceHolder.Callback {
         //
         mPulse = new Pulse();
         mPulse.initPulse();
+        //
+        mScaleGestureDetector = new ScaleGestureDetector(context, mScaleGestureListener);
     }
     // 배경 그리드를 그린다.
     private void prepareBackGroundGrid() {
@@ -81,14 +87,115 @@ public class ScopePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        super.onTouchEvent(event);
+        boolean retVal = mScaleGestureDetector.onTouchEvent(event);
+        return retVal || super.onTouchEvent(event);
+    }
 
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+    private final ScaleGestureDetector.OnScaleGestureListener mScaleGestureListener
+            = new ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        private float lastSpanX;
+        private float lastSpanY;
 
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector scaleGestureDetector) {
+            lastSpanX = scaleGestureDetector.getCurrentSpanX();
+            lastSpanY = scaleGestureDetector.getCurrentSpanY();
+            if (D) Log.d(TAG, "onScaleBegin : spanX = " + lastSpanX + ", spanY = " + lastSpanY);
             return true;
         }
-        return false;
-    }
+
+        @Override
+        public boolean onScale(ScaleGestureDetector scaleGestureDetector) {
+//            float spanX = ScaleGestureDetectorCompat.getCurrentSpanX(scaleGestureDetector);
+//            float spanY = ScaleGestureDetectorCompat.getCurrentSpanY(scaleGestureDetector);
+//
+//            float distX = lastSpanX - spanX;
+//            float distY = lastSpanY - spanY;
+//
+//            float newWidth = 0.0f;
+//            float newHeight = 0.0f;
+//
+//            if (Math.abs(distX) > 5.f) {
+//                if (Math.abs(distY) > 5.f) {
+//                    newWidth = 1 * mCurrentViewport.width();
+//                } else {
+//                    newWidth = lastSpanX / spanX * mCurrentViewport.width();
+//                }
+//            } else {
+//                newWidth = 1 * mCurrentViewport.width();
+//            }
+//            if (Math.abs(distY) > 5.f) {
+//                if (Math.abs(distX) > 5.f) {
+//                    newHeight = 1 * mCurrentViewport.height();
+//                } else {
+//                    newHeight = lastSpanY / spanY * mCurrentViewport.height();
+//                }
+//            } else {
+//                newHeight = 1 * mCurrentViewport.height();
+//            }
+//
+////            float newWidth = lastSpanX / spanX * mCurrentViewport.width();
+////            float newHeight = lastSpanY / spanY * mCurrentViewport.height();
+//
+//            Log.d("ILineGraphView", "distX = " + distX + ", distY = " + distY + ", newWidth = " + newWidth + ", newHeight = " + newHeight);
+//            float focusX = scaleGestureDetector.getFocusX();
+//            float focusY = scaleGestureDetector.getFocusY();
+//            hitTest(focusX, focusY, viewportFocus);
+//
+//            mCurrentViewport.set(
+//                    viewportFocus.x
+//                            - newWidth * (focusX - mContentRect.left)
+//                            / mContentRect.width(),
+//                    viewportFocus.y
+//                            - newHeight * (mContentRect.bottom - focusY)
+//                            / mContentRect.height(),
+//                    0,
+//                    0);
+//            mCurrentViewport.right = mCurrentViewport.left + newWidth;
+//            mCurrentViewport.bottom = mCurrentViewport.top + newHeight;
+//            constrainViewport();
+//            ViewCompat.postInvalidateOnAnimation(InteractiveLineGraphView.this);
+//
+//            lastSpanX = spanX;
+//            lastSpanY = spanY;
+            return true;
+        }
+
+        @Override
+        public void onScaleEnd(ScaleGestureDetector scaleGestureDetector) {
+
+            float spanX = scaleGestureDetector.getCurrentSpanX();
+            float spanY = scaleGestureDetector.getCurrentSpanY();
+            if (D) Log.d(TAG, "onScaleEnd : spanX = " + spanX + ", spanY = " + spanY);
+
+            float distX = lastSpanX - spanX;
+            float distY = lastSpanY - spanY;
+
+            if (Math.abs(distX) > 50.f) {
+                if (Math.abs(distY) > 50.f) {
+                    // 가로이동과 세로이동이 모두 유효한 크기이면 대각선 핀치 줌 동작으로 보고 무시
+                } else {
+                    // 가로이동만 유효한 크기이면 수평 스케일 동작으로 인식.
+
+                }
+            } else {
+                // 가로이동이 유효한 크기 이하이면 무시.
+            }
+            if (Math.abs(distY) > 50.f) {
+                if (Math.abs(distX) > 50.f) {
+                    // 가로이동과 세로이동이 모두 유효한 크기이면 대각선 핀치 줌 동작으로 보고 무시
+                } else {
+                    // 세로이동만 유효한 크기이면 수직 스케일 동작으로 인식.
+
+                }
+            } else {
+                // 세로이동이 유효한 크기 이하이면 무시.
+            }
+            Log.d("ILineGraphView", "distX = " + distX + ", distY = " + distY);
+
+
+        }
+    };
 
     class DrawThread extends Thread {
         Handler mHandler;
